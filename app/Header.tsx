dfbf2f83c72dba88fb4f6a695e906a88774b0170
ryan-didevar <abdidevar@gmail.com>
2023-12-18 11:24:40 +0330

    Searchbar added

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import { HiSearch } from "react-icons/hi";
 
 const Header = () => {
   return (
@@ -21,6 +21,17 @@ const Header = () => {
             className="object-contain"
           />
         </div>
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <HiSearch className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        <h4>Right</h4>
       </div>
     </div>
   );
